92f515750dbc74320cd02caf8aa620728978f515
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-03-19 13:49:21 -0300

    quantidade do caracteres do texto arrumado

--- caracteres.c
+++ caracteres.c
@@ -2,21 +2,21 @@
 #include <string.h>
 #include <ctype.h>
 
-#define caracteresTotal 256
+#define caracteresTotal 100
 
 void contarCaracteres(const char *texto) {
 
     int frequencia[256] = {0};
     int linha;
-    
-    for (int linha = 0; linha < 256; linha++) {
-        if (frequencia[linha] > 0) {
-            printf("Quatidade de caracteres no texto: %d\n", frequencia[linha]);
-        }
-    }
 
     for (linha = 0; linha < strlen(texto); linha++) {
         frequencia[(unsigned char)tolower(texto[linha])]++;
+    }
+    
+    for (int linha = 0; linha < 256; linha++) {
+        if (frequencia[linha] > 0) {
+            printf("Quantidade de caracteres: %d\n", frequencia[linha]);
+        }
     }
 }
 

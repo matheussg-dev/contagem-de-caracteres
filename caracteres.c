8e8b15ece5f531f8f4f24335a7bbf567a60eaa99
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-03-19 13:15:36 -0300

    quantidade do caracteres do texto teste

--- caracteres.c
+++ caracteres.c
@@ -2,12 +2,18 @@
 #include <string.h>
 #include <ctype.h>
 
-#define caracteresTotal 255
+#define caracteresTotal 256
 
 void contarCaracteres(const char *texto) {
 
     int frequencia[256] = {0};
     int linha;
+    
+    for (int linha = 0; linha < 256; linha++) {
+        if (frequencia[linha] > 0) {
+            printf("Quatidade de caracteres no texto: %d\n", frequencia[linha]);
+        }
+    }
 
     for (linha = 0; linha < strlen(texto); linha++) {
         frequencia[(unsigned char)tolower(texto[linha])]++;
